bba53471e38e6a7e1e327c938fdf62b00516d456
Ivan Martinez von Halle <ivanmvh@gmail.com>
2022-11-28 17:21:34 -0500

    Add showlist function

--- app.js
+++ app.js
@@ -10,10 +10,26 @@ addBook = () => {
         title: title.value,
         author: author.value
     }
-    books.push(newBook)
+    books.push(newBook);
+    showList();
     localStorage.setItem('books', JSON.stringify(books));
 }
 
+function showList() {
+    let listHtml = '';
+    for (let i = 0; i < books.length; i += 1) {
+      listHtml += `
+        <div class="book-row" id="book-${i}">
+          <p>${books[i].title}</p>
+          <p>${books[i].author}</p>
+          <button id="btn-book-${i}" data-index=${i} class="btn-remove">Remove</button> 
+          <hr>
+        </div>`;
+    }
+    listSection.innerHTML = listHtml;
+  }
 
-addButton.addEventListener('click', addBook)
 
+addButton.addEventListener('click', addBook);
+
+showList();
