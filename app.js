2edd41de98d640e337f55f96de7ed7a17a35dd9a
Ivan Martinez von Halle <ivanmvh@gmail.com>
2022-11-29 15:31:52 -0500

    Create class UI

--- app.js
+++ app.js
@@ -1,52 +1,55 @@
-let books = JSON.parse(localStorage.getItem('books')) || [];
-const listSection = document.getElementById('list-sec');
-const addButton = document.getElementById('btn-add');
-const title = document.getElementById('title');
-const author = document.getElementById('author');
-let removeButtons = document.querySelectorAll('.btn-remove');
 
-/*
-render list
-*/
-function showList() {
-  let listHtml = '';
-  for (let i = 0; i < books.length; i += 1) {
-    listHtml += `
+const addButton = document.getElementById('btn-add');
+
+class UI {
+    constructor (books) {
+        books = JSON.parse(localStorage.getItem('books')) || [];
+        removeButtons = document.querySelectorAll('.btn-remove');
+        listSection = document.getElementById('list-sec');
+        title = document.getElementById('title');
+        author = document.getElementById('author');
+    }
+
+    static showList() {
+        let listHtml = '';
+        for (let i = 0; i < this.books.length; i += 1) {
+        listHtml += `
           <div class="book-row" id="book-${i}">
-            <p>${books[i].title}</p>
-            <p>${books[i].author}</p>
+            <p>${this.books[i].title}</p>
+            <p>${this.books[i].author}</p>
             <button id="btn-book-${i}" data-index=${i} class="btn-remove">Remove</button> 
             <hr>
           </div>`;
-  }
-  listSection.innerHTML = listHtml;
-  addRevomeEvents(); /* eslint-disable-line */
-}
+        }
+        this.listSection.innerHTML = listHtml;
+        addRevomeEvents(); /* eslint-disable-line */
+    }
 
-const addBook = () => {
-  const newBook = {
-    title: title.value,
-    author: author.value,
-  };
-  books.push(newBook);
-  showList();
-  localStorage.setItem('books', JSON.stringify(books));
-};
+    static addBook  ()  {
+        const newBook = {
+          title: title.value,
+          author: author.value,
+        };
+        this.books.push(newBook);
+        this.showList();
+        localStorage.setItem('books', JSON.stringify(this.books));
+      };
+      
+    static removeBook (e)  {
+        const titleToRemove = e.currentTarget.previousElementSibling.previousElementSibling.textContent;
+        this.books = this.books.filter((book) => this.book.title !== titleToRemove);
+        this.showList();
+        localStorage.setItem('books', JSON.stringify(this.books));
+      };      
 
-const removeBook = (e) => {
-  const titleToRemove = e.currentTarget.previousElementSibling.previousElementSibling.textContent;
-  books = books.filter((book) => book.title !== titleToRemove);
-  showList();
-  localStorage.setItem('books', JSON.stringify(books));
-};
+    static addRevomeEvents() {
+        this.removeButtons = document.querySelectorAll('.btn-remove');
+        this.removeButtons.forEach((button) => {
+          button.addEventListener('click', (e) => this.removeBook(e));
+        });
+    };     
+}    
 
-addButton.addEventListener('click', addBook);
+addButton.addEventListener('click', UI.addBook);
 
-const addRevomeEvents = () => {
-  removeButtons = document.querySelectorAll('.btn-remove');
-  removeButtons.forEach((button) => {
-    button.addEventListener('click', (e) => removeBook(e));
-  });
-};
-
-showList();
+UI.showList();
